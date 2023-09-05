5f3da9f08033f8306c4a6af25db55366a41502cd
Your Name <you@example.com>
2023-09-05 09:16:13 -0600

    fixed bugs

--- src/main/java/fmoues/edu/sv/sistemascontable/Controllers/Login.java
+++ src/main/java/fmoues/edu/sv/sistemascontable/Controllers/Login.java
@@ -1,5 +1,6 @@
 package fmoues.edu.sv.sistemascontable.Controllers;
 
+import fmoues.edu.sv.sistemascontable.Models.Usuarios;
 import fmoues.edu.sv.sistemascontable.Services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,9 +11,16 @@ public class Login {
    @Autowired
     private AuthService auth;
 
+
+
    @PostMapping("/")
     public boolean login(@RequestBody String username, @RequestBody String password){
        return auth.getAuth(username,password) != null ? auth.getAuth(username,password).isEstado_usuario():false;
+   }
+
+   @PostMapping("/resgister")
+    public boolean register(@RequestBody Usuarios user){
+       return auth.registro(user);
    }
 
 }
--- src/main/java/fmoues/edu/sv/sistemascontable/Services/AuthService.java
+++ src/main/java/fmoues/edu/sv/sistemascontable/Services/AuthService.java
@@ -2,16 +2,29 @@ package fmoues.edu.sv.sistemascontable.Services;
 
 import fmoues.edu.sv.sistemascontable.Models.Usuarios;
 import fmoues.edu.sv.sistemascontable.Repositorys.UsuariosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+    @Autowired
     private UsuariosRepository users;
 
     public Usuarios getAuth(String username, String password){
         Usuarios user = users.findUserByUsername(username);
         Usuarios res = null;
         if(user.getContrasena_usuario() == password) res = user;
+        return res;
+    }
+
+    public boolean registro (Usuarios user){
+        boolean res;
+        try {
+            users.save(user);
+            res = true;
+        }catch (Error e){
+            res = false;
+        }
         return res;
     }
 }

4de956de8091f9dac32c707da7e895dd19d91339
Cristian Pineda <cristian.pineda.1997@outlook.com>
2023-09-07 23:52:46 -0600

    add getsubcuentas

--- src/main/java/fmoues/edu/sv/sistemascontable/Controllers/CatalogoController.java
+++ src/main/java/fmoues/edu/sv/sistemascontable/Controllers/CatalogoController.java
@@ -1,23 +1,29 @@
 package fmoues.edu.sv.sistemascontable.Controllers;
 
+import com.google.gson.Gson;
 import fmoues.edu.sv.sistemascontable.Services.CatalogoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/catalogo")
 public class CatalogoController {
 
     @Autowired
     private CatalogoService misServicios;
+    @Autowired
+    private Gson gson;
 
     public CatalogoController() {
         this.misServicios = new CatalogoService();
+    }
+
+    @GetMapping("/subcuentas")
+    public String getSubCuentas() {
+        return gson.toJson(this.misServicios.getCatalogo());
     }
 
     @GetMapping("/cuentas/{id}")
